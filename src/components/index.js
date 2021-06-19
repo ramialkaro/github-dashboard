04487ec8c2bbda5cc60f832743ea5b59cf2ce4aa
ramialkaro <rami.alkaro@gmail.com>
2021-06-19 23:44:03 +0300

    remove the topbar from app

--- src/components/index.js
+++ src/components/index.js
@@ -4,18 +4,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { mainListItems } from './listItems';
 import Chart from './Chart';
 import CalcRepos from './CalcRepos';
@@ -36,7 +30,7 @@ function Copyright() {
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
   },
@@ -73,8 +67,8 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
-    color:'#000',
-    fontWeight:'bold',
+    color: '#000',
+    fontWeight: 'bold',
   },
   drawerPaper: {
     position: 'relative',
@@ -119,37 +113,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DashBoard() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const user = 'ramialkaro'
+  const open = false;
+  const user = 'ramialkaro';
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h5"  noWrap className={classes.title}>
-            Github Dashboard
-          </Typography>
-          
-        </Toolbar>
-      </AppBar>
+
       <Drawer
         variant="permanent"
         classes={{
@@ -157,12 +129,6 @@ export default function DashBoard() {
         }}
         open={open}
       >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
         <List>{mainListItems(user)}</List>
         {/* <Divider />
         <List>{secondaryListItems}</List> */}
