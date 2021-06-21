7db914d6c8d30e73ce9975760334ef864f17712a
ramialkaro <rami.alkaro@gmail.com>
2021-06-21 11:39:05 +0300

    use owner url

--- src/components/CalcRepos.js
+++ src/components/CalcRepos.js
@@ -3,9 +3,9 @@ import CountUp from 'react-countup';
 import { Link, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Title from './Title';
-import FetchData from './FetchData';
+import FetchData from '../FetchData';
 import { formatZonedDate } from '../utils';
-import { BASE_URL, REPOS_URL } from '../constants';
+import { OWNER_ACCOUNT, REPOS_URL } from '../constants';
 
 const useStyles = makeStyles(theme => ({
   depositContext: {
@@ -33,7 +33,7 @@ export default function CalcRepos() {
         Last created at {data[0] !== undefined ? formatZonedDate(data[0].created_at) : null}
       </Typography>
       <div>
-        <Link color="primary" href={BASE_URL}>
+        <Link color="primary" href={OWNER_ACCOUNT}>
           View your account
         </Link>
       </div>
