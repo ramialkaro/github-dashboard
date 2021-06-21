fef52458a056ea880699f9cbe43f7992115e176a
ramialkaro <rami.alkaro@gmail.com>
2021-06-21 11:38:33 +0300

    add more constants variables.

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
 export const OWNER = 'ramialkaro';
+export const OWNER_ACCOUNT = 'https://github.com/' + OWNER;
+export const DEVELOPER_ACCOUNT = 'https://github.com/ramialkaro'; // this will be used for copyright section 
 export const BASE_URL = 'https://api.github.com/users/' + OWNER;
 export const REPOS_URL = BASE_URL + '/repos';
+export const FOLLOWERS = BASE_URL + '/followers';
