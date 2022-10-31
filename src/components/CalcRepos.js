e9538fbe41a2b66306db8b4276c63bd178bb6657
Rami Al-Karo <rami@lamia.fi>
2022-10-31 08:57:31 +0200

    fetch correct repos amount

--- src/components/CalcRepos.js
+++ src/components/CalcRepos.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CountUp from 'react-countup';
 import { Link, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import FetchData from '../FetchData';
 import { formatZonedDate } from '../utils';
-import { OWNER_ACCOUNT, REPOS_URL } from '../constants';
+import { BASE_URL, OWNER_ACCOUNT, REPOS_URL } from '../constants';
+import { useState } from 'react';
 
 const useStyles = makeStyles(theme => ({
   depositContext: {
@@ -16,25 +17,31 @@ const useStyles = makeStyles(theme => ({
 export default function CalcRepos() {
   const classes = useStyles();
   const { data } = FetchData(REPOS_URL);
-  data.sort((a, b) => {
-    if (a.created_at < b.created_at) return 1;
-    if (a.created_at > b.created_at) return -1;
-    else return 0;
-  });
+  const baseInfo = FetchData(BASE_URL);
+  const [publicRepos, setPublicRepos] = useState(0)
+  useEffect(() => {
+    setPublicRepos(baseInfo.data['public_repos'])
+  }, [baseInfo]);
 
-  return (
-    <React.Fragment>
-      <Typography component="p" variant="h5">
-        {<CountUp start={0} end={data.length} duration={2.75} />} public repository
-      </Typography>
-      <Typography color="textSecondary" className={classes.depositContext}>
-        Last created at {data[0] !== undefined ? formatZonedDate(data[0].created_at) : null}
-      </Typography>
-      <div>
-        <Link color="primary" href={OWNER_ACCOUNT}>
-          View your account
-        </Link>
-      </div>
-    </React.Fragment>
-  );
+data.sort((a, b) => {
+  if (a.created_at < b.created_at) return 1;
+  if (a.created_at > b.created_at) return -1;
+  else return 0;
+});
+
+return (
+  <React.Fragment>
+    <Typography component="p" variant="h5">
+      {publicRepos ? <CountUp start={0} end={publicRepos} duration={2.75} /> : null } public repository
+    </Typography>
+    <Typography color="textSecondary" className={classes.depositContext}>
+      Last created at {data[0] !== undefined ? formatZonedDate(data[0].created_at) : null}
+    </Typography>
+    <div>
+      <Link color="primary" href={OWNER_ACCOUNT}>
+        View your account
+      </Link>
+    </div>
+  </React.Fragment>
+);
 }
